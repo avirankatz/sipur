837eab1fd341a53a5cf2128ab87feabc82f94073
Aviran Katz <aviran.katz@gmail.com>
2018-07-04 23:04:58 +0300

    add clear API

--- server.js
+++ server.js
@@ -12,5 +12,9 @@ app.post('/line', (req, res) => {
     lines.push(req.body.line);
     res.json(200);
 });
+app.get('/clear', (req, res) => {
+    lines.length = 0;
+    res.json(200);
+})
 app.use(express.static('./dist'));
 app.listen(3030);
